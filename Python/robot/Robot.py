bbda781b29dd1002a3295396c7356af4bd1254d9
Rafaelmos <rafael.rjms0@gmail.com>
2021-04-25 19:05:18 -0300

    update Robot

--- Python/robot/Robot.py
+++ Python/robot/Robot.py
@@ -1,82 +1,81 @@
 nome = "Robozinho"
-posicao = [0,0]
-direcao = "Norte"
+posicao = [0, 0]
+direcao = ["Norte"]
+
 
 def iniciarRobot():
     print(f"""{nome} inicou
     a posição atual do {nome} é: {posicao}
-    a direção atual do {nome} é: {direcao} """)
-    
+a direção atual do {nome} é: {direcao[0]} """)
+
+
+
 def statusRobot():
     print(f"""A posição atual do {nome} é: {posicao}
-    a direção atual do {nome} é: {direcao}""")
+S direção atual do {nome} é: {direcao[0]} """)
+1
 
 def andarUmPasso():
     print(posicao)
-    if (direcao is "Norte"):
-        posicao[1]+=1
-        return posicao
-    elif (direcao=="Sul"):
-        posicao[1]-=1
-        return posicao
-    elif (direcao=="Leste"):
-        posicao[0]+=1
-        return posicao
-    elif (direcao=="Oeste"):
-        posicao[0]-=1
-        return posicao
+    if (direcao == "Norte"):
+        posicao[1] += 1
+    elif (direcao == "Sul"):
+        posicao[1] -= 1
+    elif (direcao == "Leste"):
+        posicao[0] += 1
+    elif (direcao == "Oeste"):
+        posicao[0] -= 1
+    print(f"O {nome} andou")
+
 
 def andarVariosPasso():
     andar = int(input("Digite quantos passos você quer andar: "))
-    if (direcao is "Norte"):
-        posicao[1]+=1
-    elif (direcao=="Sul"):
-        posicao[1]-=1
-    elif (direcao=="Leste"):
-        posicao[0]+=1
-    elif (direcao=="Oeste"):
-        posicao[0]-=1
+    if (direcao == "Norte"):
+        posicao[1] += andar
+    elif (direcao == "Sul"):
+        posicao[1] -= andar
+    elif (direcao == "Leste"):
+        posicao[0] += andar
+    elif (direcao == "Oeste"):
+        posicao[0] -= andar
+    print(f"O {nome} andou")
+
 
 def mudarPosicao():
     x = int(input("Digite a posição x: "))
     y = int(input("Digite a posição y: "))
-    posicao[0]=x
-    posicao[1]=y
+    posicao[0] = x
+    posicao[1] = y
+    print(f"A nova posição é: {posicao}")
 
 def mudarDirecao():
-        print("""ESCOLHA A NOVA DIREÇÃO
-        [0] = Norte
-        [1] = Sul
-        [2] = Leste
-        [3] = Oeste""")
-
-        novadirecao = int(input("Escolha a opção: "))
-
-        if novadirecao == 0:
-            direcao = "Norte"
-            print(f"A nova direção atual é {direcao}")
-        elif novadirecao == 1:
-            direcao = "Sul"
-            print(f"A nova direção atual é {direcao}")
-        elif novadirecao == 2:
-            direcao = "Leste"
-            print(f"A nova direção atual é {direcao}")
-        elif novadirecao == 3:
-            direcao = "Oeste"
-            print(f"A nova direção atual é {direcao}")
-        else:
-            print("OPÇÃO INVALIDA, ESCOLHA UMA OPÇÃO CORR3ETA")
-            mudarDirecao()
+    print("""ESCOLHA A NOVA DIREÇÃO
+[0] = Norte
+[1] = Sul
+[2] = Leste
+[3] = Oeste""")
+    escolher = int(input("Escolha a opção: "))
+    if escolher == 0:
+        direcao[0]=["Norte"]
+    elif escolher == 1:
+        direcao[0]=["Sul"]
+    elif escolher == 2:
+        direcao[0]=["Leste"]
+    elif escolher == 3:
+        direcao[0]=["Oeste"]
+    else:
+        print("OPÇÃO INVALIDA, ESCOLHA UMA OPÇÃO CORRETA")
+        mudarDirecao()
+    print(f"A nova direção atual é {direcao[0]}")
 
 def retornaPosZero( ):
     posicao = [0,0]
-    direcao = "Norte"
+    direcao[0] = "Norte"
 
+# iniciarRobot()
+#andarUmPasso()
+# andarVariosPasso()
+#mudarPosicao()
+mudarDirecao()
 
-# iniciarRobot()
-# andarUmPasso()
-# iniciarRobot()
-# mudarPosicao()
-# iniciarRobot()
-# mudarDirecao()
-# statusRobot()
+statusRobot()
