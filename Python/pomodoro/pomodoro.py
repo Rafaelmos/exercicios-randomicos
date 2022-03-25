82e13a72c2628251df6c9faaf339865c73d7aea2
Rafaelmos <rafael.rjms0@gmail.com>
2022-03-25 14:22:42 -0300

    adding an alarm

--- Python/pomodoro/pomodoro.py
+++ Python/pomodoro/pomodoro.py
@@ -1,16 +1,39 @@
 import time
+import pygame
+import os
 
-def work(seconds):
-    print ("já")
-    time.sleep(seconds)
-    return print("Acabou, hora de descançar")
 
-def relax(seconds):
+working = float(input("Digite o tempo de estudo/trabalho: "))
+relaxing = float(input("Digite o tempo de descanço: "))
+
+def work (minutes):
+    seconds = minutes * 60
     time.sleep(seconds)
+    alarm()
+    print("Acabou, hora de descançar")  
+    return relax(relaxing) #Usar para setar o tempo de descanço
+
+def relax(minutes):
+    seconds = minutes * 60
+    time.sleep(seconds)
+    alarm()
     return print("Acabou, hora de estudar")
 
-work() #Usar para setar o tempo de estudo
-relax() #Usar para setar o tempo de descanço
+def alarm():
+    pygame.mixer.init()
+    pygame.init()
+    sourceFileDir = os.path.dirname(os.path.abspath(__file__))
+    filePath = os.path.join(sourceFileDir, 'alarmclock.mp3')
+    pygame.mixer.music.load(filePath)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+    return
+
+work(working) #Usar para setar o tempo de estudo
+
+
+
+
 
 
 
