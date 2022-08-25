c9f16916134094a626f707ca9967515bf2d7c869
Rafael Moura <65968331+Rafaelmos@users.noreply.github.com>
2022-08-24 23:35:59 -0300

    redução do código e correção de bug
    Havia um bug onde a um índice se somava com o mesmo índice, onde isso não deveria ocorrer.

--- Python/checker-sum-of-two-values-in-a-list/checker-sum-of-two-values.py
+++ Python/checker-sum-of-two-values-in-a-list/checker-sum-of-two-values.py
@@ -1,27 +1,15 @@
-def check_sum_two_values():
-    try:
-        number_checker = int(input("Digite o número a ser checado: "))
-        scan_result = False
-        while len(list_any) != 0 and scan_result == False:
-            index = 0
-            while index != len(list_any) and scan_result == False:
-                current_value = list_any[index]
-                last_value = list_any[-1]
-                number_verified = current_value+last_value
-                index = index + 1
-                if number_verified == number_checker:
-                    scan_result = True
-                    del(list_any[:])
-                else:
-                    scan_result = False
-            if len(list_any) == 0:
-                scan_result = True
-            else:
-                del(list_any[-1])
-        print(scan_result)
-    except NameError:
+def check_sum_two_values(list_any):
+  number_checker = int(input("Digite o número a ser checado: "))
+  try:
+      for i in range(len(list_any)): 
+        index = i + 1
+        while(len(list_any) > index):
+          if(list_any[i] + list_any[index] == number_checker):
+            return True
+          index += 1
+  except NameError:
         print("Crie uma lista de números inteiros com a variavel chamada de 'list_any' ")
-
-
+  return False
+  
 list_any = [2, 3, 4, 5, 1]
-check_sum_two_values()
+print(check_sum_two_values(list_any))
